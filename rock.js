5394ee60484da44c506cbd07f32e02bd5b903171
Logan <logan.nelson012@gmail.com>
2022-10-03 14:05:10 -0700

    Made a check score feature that returns the round result as well as the winner of the game.

--- rock.js
+++ rock.js
@@ -53,11 +53,29 @@ function getPlayerChoice(){
 }
 
 function game(){
+    let scorePlayer = 0;
+    let = scoreComputer = 0;
     for (let i = 0; i < 5; i++){
         const playerSelection = getPlayerChoice();
         const computerSelection = getComputerChoice();
         console.log(playRound(playerSelection , computerSelection));
+        if(checkWinner(playerSelection , computerSelection) == "Player"){
+            scorePlayer++;
+        }
+        else if(checkWinner(playerSelection , computerSelection) == "Computer"){
+            scoreComputer++;
+        }
+        if(scorePlayer > scoreComputer){
+            console.log("You won, this is your biggest achievement to date");
+        }
+        else if(scorePlayer < scoreComputer){
+          console.log("You lose, you should be ashamed.");  
+        }
+        else{
+            console.log('You tied, try again.');
+        }
     }
+    console.log("Game Over")
 }
 
 game()
